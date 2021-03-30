04013d2adca039fdc5f84ba81457d37ecb637440
Bartek <zgredinzyyy@gmail.com>
2021-03-30 10:19:52 +0200

    Upload script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         Odrabiamy.pl Watermark Remover
+// @namespace    https://*.odrabiamy.pl/
+// @version      0.1
+// @description  Smacznej kawusi jebać kapusi
+// @author       zgredinzyyy
+// @match        https://odrabiamy.pl/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+      setInterval(() => {
+          var elems = [].filter.call( document.getElementsByTagName("div"), function( input ) {
+              if (input.className == "watermark") {
+                  input.innerHTML = "";
+              }
+          });
+      }, 1000)
+})();
